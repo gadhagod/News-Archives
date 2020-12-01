8ede4d9d2ad540f0962c57ef0ffe96c86571aec9
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-12-01 10:52:28 -0800

    Redirect to new page when searching in demo

--- src/static/demo.js
+++ src/static/demo.js
@@ -2,17 +2,17 @@ function search_day(){
     var day = document.getElementById('day').value;
     document.write('loading..');
     if (day === ''){
-        window.location.replace('demo/');
+        window.location.href = 'demo/';
     } else {
-        window.location.replace('demo/day/' + day);
+        window.location.href = 'demo/day/' + day;
     }
 }
 function search_keyword(){
     var keyword = document.getElementById('keyword').value;
     document.write('loading..');
     if (keyword === ''){
-        window.location.replace('demo/');
+        window.location.href = 'demo/';
     } else {
-        window.location.replace('demo/keyword/' + keyword);
+        window.location.href ='demo/keyword/' + keyword;
     }
 }
