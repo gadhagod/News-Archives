9a02cdcc79d5b5a1febb84c475a1ac7f779f30ea
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-12-04 13:26:11 -0800

    Fix file extensions, GH pages redirect

--- src/demo.py
+++ src/demo.py
@@ -2,11 +2,10 @@ from flask import render_template
 import newsarchives as archiveslib
 
 def index():
-    return(render_template('demo.jinja'))
+    return(render_template('demo.html'))
 
 def day_search(day):
     return(render_template('demo_search.jinja', news=archiveslib.day(day)['data'][0]['articles']))
 
 def keyword_search(target):
     return(render_template('demo_search.jinja', news=archiveslib.keyword(target, limit=20)['data'], keyword=target))
-    
